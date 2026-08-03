8e5c36dd3d9c0aebee9dea2c0353d71c3c6419d6
mariasantosrodrigues28-art <maria.santos.rodrigues28@escola.pr.gov.br>
2026-08-03 11:22:44 -0300

    2

---
+++ script.js
@@ -0,0 +1,19 @@
+const-botoes-=-document.querySelectorA11-`button`;
+
+botoes.forEach(function botao) {
+    let-curtiu-=-false;
+    botao.addEventListener"click",botaoClicado;
+    function-botaoClicado-;
+    console.console.log(achou);
+    let-texto-=-botao.querySelector=span;
+    if-curtiu-==-false
+    texto.textContent--;
+    curtiu-=-true;
+    else-
+    texto.text
+    
+
+    
+} => {
+    
+});
